951c7c5cd585b8d08374cdb3334160fde36c99f0
Gastón Fernández <gastonfc@gmail.com>
2013-10-27 19:42:49 -0700

    Edit and update added to movie.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,17 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was syccessfully created."
     redirect_to movies_path
   end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} has been updated."
+    redirect_to movie_path(@movie)
+  end
 end
 
 
